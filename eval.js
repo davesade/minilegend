4f6a3fe1c99c937f6040be9e7a1fa96a7397d4f5
davesade <davesade.42@gmail.com>
2018-02-25 23:58:31 +0100

    Button for evaluation added.

--- eval.js
+++ eval.js
@@ -1,11 +1,97 @@
-function battle(playerA, playerB, callback) {
-    console.log(JSON.stringify(playerA));
-    console.log(JSON.stringify(playerB));
+function battle(players, callback) {
+
+  console.log(JSON.stringify(players));
+
+  let combinations = {
+    archer: {
+      name: "archer",
+      defeats: ["soldier", "knight"]
+    },
+    spy: {
+      name: "spy",
+      defeats: ["archer", "horseman"]
+    },
+    soldier: {
+      name: "soldier",
+      defeats: ["spy", "knight"]
+    },
+    knight: {
+      name: "knight",
+      defeats: ["spy", "horseman"]
+    },
+    horseman: {
+      name: "horseman",
+      defeats: ["soldier", "archer"]
+    }
+  };
+
+  let cards=[];
+  players[0].score = 0;
+  players[1].score = 0;
+
+  for (let i = 0; i < players.length; i++){
+    cards.push(players[i].cards);
+  }
+
+  var result  = {
+
+    message : "ok",
+    playerAhistory : [],
+    playerBhistory : [],
+    draw : false,
+    playerAwinner: false,
+    playerBwinner: false
+
+  }
+
+
+  console.log(cards);
+
+  function giveScore() {
+    for (let turn = 0; turn < deckSize; turn++ ){
+      if (cards[0][turn] === cards[1][turn]){
+        console.log('DRAW');
+        result.playerAhistory.push(-1);
+        result.playerBhistory.push(-1);
+      } else {
+        console.log(cards[0][turn]);
+        console.log(cards[1][turn]);
+        let leadCard = combinations[cards[0][turn]]
+        let victory = leadCard.defeats.indexOf(cards[1][turn]) > -1;
+        if (!victory){
+          console.log(players[0].name + " wins with " + cards[0][turn] + " defeating " + cards[1][turn] + " of " + players[1].name + ".");
+          result.playerAhistory.push(1);
+          result.playerBhistory.push(0);
+          players[0].score++;
+        } else {
+          console.log(players[1].name + " wins with " + cards[1][turn] + " defeating " + cards[0][turn] + " of " + players[0].name + ".");
+          players[1].score++;
+          result.playerAhistory.push(0);
+          result.playerBhistory.push(1);
+        }
+        console.log(victory);
+
+      }
+    };
+
+  }
+
+  giveScore();
+
+  if (players[0].score > players[1].score) {
+    result.playerAwinner = true;
+  } else {
+    if (players[0].score > players[1].score) {
+      result.playerBwinner = true;
+    } else {
+      result.draw = true;
+    }
+  }
 
 
 
 
-    var result = "yes";
-    callback(null, result);
+
+  callback(null, result);
 
 }
--- main.js
+++ main.js
@@ -1,32 +1,40 @@
-const deckSize = 5;
-const maxPlayers = 2;
-var players = [];
-var fight = [];
+  const deckSize = 5;
+  const maxPlayers = 2;
+  var players = [];
+  var fight = [];
 
-function preload() {
-
-}
-
-function setup() {
-
-    for (var i = 0; i < maxPlayers; i++) {
-        players[i] = new Player(deckSize)
+  function preload() {
+    if (maxPlayers / 2 === maxPlayers % 2 ){
+      console.log('Players must be in pair!')
     }
+  }
 
-    createP(JSON.stringify(players));
+  function setup() {
 
-    battle(players[0], players[1], function(err, result){
-        if (err) {
-            console.log('Something went wrong!')
-        } else {
-            console.log(result)
-        }
+  let startButton = createButton('Start');
 
-    });
+  function renderBattle() {
+      for (var i = 0; i < maxPlayers; i++) {
+          players[i] = new Player(deckSize)
+      }
 
-}
+      createP(JSON.stringify(players));
 
-function draw() {
+      battle(players, function(err, result){
+          if (err) {
+              console.log('Something went wrong!')
+          } else {
+              createP(JSON.stringify(result));
+              doTheGraphics(result);
+          }
+
+      });
+  }
+
+  startButton.mousePressed(renderBattle);
+  }
+
+  function draw() {
 
 
-}
+  }
