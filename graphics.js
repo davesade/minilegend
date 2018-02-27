0bf844238498cb5fd4911f2085372fe93ea6f163
davesade <davesade.42@gmail.com>
2018-02-27 23:11:39 +0100

    Improved graphics display.

--- graphics.js
+++ graphics.js
@@ -3,26 +3,26 @@ function doTheGraphics(result, players) {
   let gameFrame = createDiv('');
   gameFrame.addClass('gameFrame');
 
-  for (let j = 0; j < players.length+1; j++) {
-console.log('here');
+  for (let j = 0; j < players.length; j++) {
+
     let gameRow = createDiv('');
     gameRow.addClass('gameRow');
-    //gameRow.parent('.gameFrame');
+    //gameRow.parent('gameFrame');
 
     for (let i = 0; i < deckSize+1; i++){
       let cardDiv = createDiv('');
       cardDiv.addClass(players[j].cards[i]);
-      //cardDiv.parent('.gameRow')
-      }
-      let nameDiv = createDiv(players[j].name);
-      nameDiv.addClass('name');
-      //nameDiv.parent('.gameRow');
-      let scoreDiv = createDiv('');
-
-      if (result.playerAwinner) {
-        scoreDiv.addClass('winner');
-        //scoreDiv.parent('.gameRow');
+      //cardDiv.parent('gameRow')
     }
 
+    let nameDiv = createDiv(players[j].name);
+    nameDiv.addClass('name');
+    //nameDiv.parent('gameRow');
+
+    if (result.playerAwinner) {
+      let scoreDiv = createDiv('');
+      scoreDiv.addClass('winner');
+      //scoreDiv.parent('gameRow');
+    }
   }
 }
